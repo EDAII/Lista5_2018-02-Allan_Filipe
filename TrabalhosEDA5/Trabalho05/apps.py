de46e03e2be9da1213379e08addb5c850a46d70c
FilipeKN4 <filipekn4@gmail.com>
2018-10-15 22:15:30 -0300

    Add project initial structure

---
+++ TrabalhosEDA5/Trabalho05/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Trabalho05Config(AppConfig):
+    name = 'Trabalho05'
---
+++ TrabalhosEDA5/Trabalho05/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+def home(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        byte_str = myfile.file.read()
+
+        # Convert to a "unicode" object
+        text_obj = byte_str.decode('UTF-8')
+        columns_descriptions, all_data = read_csv(text_obj.splitlines())
+
+        return render(request, 'result.html', {'columns_descriptions': columns_descriptions})
+    else:
+        # Nothing to do
+        pass
+
+    return render(request, 'home.html')
+
+
+def read_csv(file):
+    all_data = []
+    columns_descriptions = []
+
+    # Save all csv data in a list of lists, removing '\n' at the last line element.
+    for line in file:
+        if not columns_descriptions:
+            columns_descriptions = line.split(",")
+            columns_descriptions[-1] = columns_descriptions[-1].strip("\n")
+        else:
+            line_splitted = line.split(",")
+            line_splitted[-1] = line_splitted[-1].strip("\n")
+            all_data.append(line_splitted)
+
+    return columns_descriptions, all_data
